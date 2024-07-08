8ec13ae9a9d99a6c99ef89bbdc937a98b97333a3
mdruuu <andrew@panocp.com>
2024-07-08 12:09:49 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { Button } from '$lib/components/ui/button';
+    import SvelteLogo from 'virtual:icons/logos/svelte-icon'
+    import PhShoppingCartBold from '~icons/ph/shopping-cart-bold'
+
+
+</script>
+<div class="bg-red-500">
+    TEST 
+</div>
+
+<Button>Click Me</Button>
+<SvelteLogo style="font-size:2em" />
+<PhShoppingCartBold style="font-size:2em" />
