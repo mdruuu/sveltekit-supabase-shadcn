d1a585fd8a0fcf73f955171ec4bb3b46217093cb
mdruuu <andrew@panocp.com>
2024-07-08 19:53:33 -0600

    tailwind styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,13 @@
 <script lang="ts">
-    import { Button } from '$lib/components/ui/button';
-    import SvelteLogo from 'virtual:icons/logos/svelte-icon'
-    import PhShoppingCartBold from '~icons/ph/shopping-cart-bold'
-
-
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-This blank project is made with the folowing: 
-<ul>
+<h1 class="text-3xl font-bold mb-4">Welcome to SvelteKit</h1>
+<p class="mb-2">This blank project is made with the following: </p>
+<ul class="list-disc pl-5">
     <li>SvelteKit-Typescript Svelte 4.0</li>
     <li>Tailwind CSS</li>
     <li>Shadcn-Svelte</li>
-    <li>Supaase</li>
+    <li>Supabase</li>
     <li>Unplugin-icons</li>
     <li>Zod</li>
 </ul>
